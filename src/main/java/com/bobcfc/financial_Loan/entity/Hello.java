2c132e1cf51ea3b4c8906394bdd7066f21cfc926
wangchunyu <wcy2572986850@163.com>
2020-03-23 16:26:14 +0800

    这是在idea中修改的文件后提交的，添加了一个Hello类

---
+++ src/main/java/com/bobcfc/financial_Loan/entity/Hello.java
@@ -0,0 +1,17 @@
+package com.bobcfc.financial_Loan.entity;
+
+/**
+ * @author wangchunyu
+ * @create 2020-03-23 16:20
+ */
+public class Hello {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
